f6a0bd5224b6185f6f9cea8edea2c0bc2848bd91
archangel7431 <aljobith@gmail.com>
2024-03-05 01:00:08 +0530

    completed the required 3 files

--- gradient_Descent/mobile_price.py
+++ gradient_Descent/mobile_price.py
@@ -1,9 +1,12 @@
+### MOBILE PRICE PREDICTION ###
+
 ## Dataset: Mobile Price prediction dataset.csv
 
 
 # Importing libraries
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from utils import gradient_descent, fista, objective_function
 
 
@@ -18,46 +21,32 @@ x = df.loc[:,["battery_power_normalised", "blue",
 
 
 # G
-g = df.loc[:,["G3"]].to_numpy()
+g = df.loc[:,["price_range_normalised"]].to_numpy()
 
 # W
-w = np.zeros((7,))
+w = np.zeros((7,1))
 
+# gradient descent and fista algorithms - comparison
 gradient, fValue_gradient, _ = gradient_descent(x, w, g, 100)
-print("OKAY")
+fista, fValue_fista = fista(x, w, g, iterations=100)
 
 
+f_gradient, _ = objective_function(x, gradient, g)
+f_fista, _ = objective_function(x, fista, g)
+print(f"The value of objective function using gradient descent : {f_gradient}")
+print(f"The value of objective function using fista : {f_fista}")
+
+if f_gradient > f_fista:
+    print("fista is better.")
+else:
+    print("gradient is better.")
 
 
-
-# fista, fValue_fista = fista(x, w, g, iterations=100)
-
-
-# f_gradient, _ = objective_function(x, gradient, g)
-# f_fista, _ = objective_function(x, fista, g)
-# print(f"The value of objective function using gradient descent : {f_gradient}")
-# print(f"The value of objective function using fista : {f_fista}")
-
-# if f_gradient > f_fista:
-#     print("fista is better.")
-# else:
-#     print("gradient is better.")
-
-
-
-# ## Gradient plot      
-# plt.plot(range(1, len(fValue_gradient) + 1), fValue_gradient, label = 'gradient descent', color ='r')
-# plt.plot(range(1, len(fValue_fista) + 1), fValue_fista, label = 'fista', color = 'b')
-# plt.xlabel("Iterations")
-# plt.ylabel("Objective function value")
-# plt.title("Iteration vs objective function value")
-# plt.legend()
-# plt.show()
-
-
-# ## fista plot
-# plt.plot(range(1, len(fValue_fista) + 1), fValue_fista)
-# plt.xlabel("Iterations")
-# plt.ylabel("Objective function value")
-# plt.title("Loss function via FISTA")
-# plt.show()
+## Objective function value plot     
+plt.plot(range(1, len(fValue_gradient) + 1), fValue_gradient, label = 'gradient descent', color ='r')
+plt.plot(range(1, len(fValue_fista) + 1), fValue_fista, label = 'fista', color = 'b')
+plt.xlabel("Iterations")
+plt.ylabel("Objective function value")
+plt.title("Iteration vs objective function value")
+plt.legend()
+plt.show()
